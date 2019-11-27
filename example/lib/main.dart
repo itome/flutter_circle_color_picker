c03f0574d82d9f21901e03ef7996b23016c4e067
Takeshi Tsukamoto <itometeam@gmail.com>
2019-11-28 01:10:16 +0900

    Implement color picker

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:flutter_circle_color_picker/flutter_circle_color_picker.dart';
 
 void main() => runApp(MyApp());
@@ -12,45 +9,26 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await FlutterCircleColorPicker.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
+  Color _currentColor = Colors.blue;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          backgroundColor: _currentColor,
+          title: const Text('Circle color picker sample'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: CircleColorPicker(
+            onChanged: _onColorChanged,
+          ),
         ),
       ),
     );
+  }
+
+  void _onColorChanged(Color color) {
+    setState(() => _currentColor = color);
   }
 }
